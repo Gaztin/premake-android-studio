c520a28fda16261badb0e6ef48019bdbcdbcb4e8
Sebastian Kylander <developer@gaztin.com>
2019-12-26 17:31:28 +0100

    Set rootProject name from workspace name

--- generators/settings.gradle.lua
+++ generators/settings.gradle.lua
@@ -9,11 +9,14 @@ androidstudio.settings_dot_gradle = m
 --
 
 function m.generate( wks )
+	p.w( 'rootProject.name = \'%s\'', wks.name )
+	p.outln( '' )
 
 	for prj in p.workspace.eachproject( wks ) do
 		local project_dir = string.format( '%s/%s', prj.location, prj.name )
 
 		p.w( 'include( \':%s\' )', prj.name )
 		p.w( 'project( \':%s\' ).projectDir = file( \'%s\' )', prj.name, p.workspace.getrelative( wks, project_dir ) )
+		p.outln( '' )
 	end
 end
