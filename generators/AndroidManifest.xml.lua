9f57f5d60b644e685545101282e67d7c26222523
Sebastian Kylander <developer@gaztin.com>
2020-07-12 20:54:34 +0200

    Add sdk versions to 'AndroidManifest.xml'

--- generators/AndroidManifest.xml.lua
+++ generators/AndroidManifest.xml.lua
@@ -10,10 +10,15 @@ androidstudio.android_manifest_dot_xml = m
 
 function m.generate( prj )
 	p.w '<?xml version="1.0" encoding="utf-8"?>'
-	p.push '<manifest xmlns:android="http://schemas.android.com/apk/res/android"'
-	p.x( 'package="%s"', prj.appid )
+	p.push '<manifest'
+	p.w 'xmlns:android="http://schemas.android.com/apk/res/android"'
+	p.w( 'package="%s"', prj.appid )
 	p.w 'android:versionCode="1"'
-	p.w 'android:versionName="1.0" >'
-	p.pop '<application/>'
+	p.w 'android:versionName="1.0">'
+	p.outln ''
+	p.w( '<uses-sdk android:minSdkVersion="%s" android:maxSdkVersion="%s" android:targetSdkVersion="%s" />', prj.minsdkversion, prj.maxsdkversion, prj.maxsdkversion )
+	p.outln ''
+	p.w '<application/>'
+	p.outln ''
 	p.pop '</manifest>'
 end
