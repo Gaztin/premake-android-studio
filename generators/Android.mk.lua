ff9803432df12e3b7463807383e730d9aa2675d8
Sebastian Kylander <developer@gaztin.com>
2020-07-12 15:07:22 +0200

    Remove old 'getrelative' hack function from 'Android.mk' generator

--- generators/Android.mk.lua
+++ generators/Android.mk.lua
@@ -75,16 +75,15 @@ end
 -- Utility functions
 --
 
-function m.getrelative( prj, dest )
-	return path.getrelative( prj.location .. '/' .. prj.name, dest )
-end
-
 function m.includeDependencies( prj )
 	for i = 1, #prj.links do
 		local link_prj = p.workspace.findproject( prj.workspace, prj.links[ i ] )
 
 		if link_prj then
-			p.w( 'include %s/%s/Android.mk', m.getrelative( prj, link_prj.location ), link_prj.name )
+			local relative_location = p.project.getrelative( prj, link_prj.location )
+			local android_mk        = path.join( relative_location, 'Android.mk' )
+
+			p.w( 'include %s', android_mk )
 		end
 	end
 end
@@ -94,7 +93,9 @@ function m.localSrcFiles( cfg )
 
 	for _, fpath in ipairs( cfg.files ) do
 		if path.iscppfile( fpath ) or path.iscfile( fpath ) then
-			table.insert( local_src_files, m.getrelative( cfg.project, fpath ) )
+			local relative_path = p.project.getrelative( cfg.project, fpath )
+
+			table.insert( local_src_files, relative_path )
 		end
 	end
 
@@ -135,9 +136,9 @@ function m.localCIncludes( cfg )
 		p.push 'LOCAL_C_INCLUDES := \\'
 
 		for i = 1, ( #cfg.includedirs - 1 ) do
-			p.w( '%s \\', m.getrelative( cfg.project, cfg.includedirs[ i ] ) )
+			p.w( '%s \\', p.project.getrelative( cfg.project, cfg.includedirs[ i ] ) )
 		end
-		p.w( '%s', m.getrelative( cfg.project, cfg.includedirs[ #cfg.includedirs ] ) )
+		p.w( '%s', p.project.getrelative( cfg.project, cfg.includedirs[ #cfg.includedirs ] ) )
 
 		p.pop ''
 	end
